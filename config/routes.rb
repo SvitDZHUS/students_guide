76cdabcc5d02330412a23ba522fcf396d8ed025d
SvitDZHUS <svitdzhus@gmail.com>
2022-03-25 21:40:13 +0200

    Styled welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'welcome_pages#welcome'
   get 'welcome', to: 'welcome_pages#welcome', as: :welcome
   get 'about', to: 'welcome_pages#about', as: :about
   get 'contacts', to: 'welcome_pages#contacts', as: :contacts
