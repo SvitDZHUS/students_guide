0fbafa348796a13110320d3a613665275c521632
SvitDZHUS <svitdzhus@gmail.com>
2022-04-08 21:53:16 +0300

    Implemented authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  enum role: { member: 0, admin: 1 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CoverUploader < CarrierWave::Uploader::Base
   storage :file
 
@@ -10,7 +12,7 @@ class CoverUploader < CarrierWave::Uploader::Base
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w(jpg jpeg png)
+    %w[jpg jpeg png]
   end
 
   # Override the filename of the uploaded files:
